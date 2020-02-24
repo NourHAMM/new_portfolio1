35954fa4e673a332280fcbfd895521e31702b911
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-02-24 10:30:03 +0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,10 @@
     <div class="relative h-screen w-screen bg-cover bg-center shadow-2xl" 
     :style="`background-image:url(${bgImage})`">
     
-     <div class="h-screen w-screen bg-red-800 opacity-75" />
+     <div class="h-screen w-screen bg-blue-200 opacity-75" />
     <div class="absolute h-screen w-screen bg top-0 flex flex-col justify-center items-center">
-      <h1 class="text-6xl font-bold text-white px-4 hover:bg-white hover:text-red-800 opacity-75">Rodolfo Roman</h1>
-      <h3 class="text-3xl font-bold text-white px-4 hover:bg-white hover:text-red-800 opacity-75">Full Stack Developer / Software Architect</h3>
+      <h1 class="text-6xl font-bold text-white px-4 hover:bg-white hover:text-yellow-200 opacity-75">Nour Hussein</h1>
+      <h3 class="text-3xl font-bold text-white px-4 hover:bg-white hover:text-yellow-200 opacity-75">Computer Information Technology Bachelors <br > Associates of Science </h3>
      </div>
      </div>
 </div>    
