a9fdf4a094b825e180525972de230deb035bc2c9
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-03-04 07:10:53 +0400

    Again

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ import todob  from '../assets/projects/02.png'
 import todot  from '../assets/projects/03.png'
 import inventory  from '../assets/projects/04.png'
 import vuetodo  from '../assets/projects/05.png'
-import vuetodo  from '../assets/projects/06.png'
+import mynotes  from '../assets/projects/06.png'
 
 
 const projects = [
@@ -45,7 +45,7 @@ const projects = [
 {
     name: 'Look at my notes!',
     description: 'A Vue.js Application with Vuex, Implementing a CRUD notes App.',
-    image: vuetodo,
+    image: mynotes,
     hashtags:['CRUD','vue.js','javascript','tailwind','vuex','vue-router'],
     url:'https://mynotes.toparadise.online/'
 },
